f6177d829fbc88afe2e100efd97968acb3206525
Nick Taylor <itsme@nicktaylor.dev>
2022-11-28 10:45:59 +0000

    change plugin setup for v3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import VisualOptions from './components'
+import { createPlugin } from "sanity"
+import React from "react"
+import VisualOptions from "./components"
 
-export default {
-  title: 'Visual Options',
-  type: "string",
-  name: 'visualOptions',
-  inputComponent: VisualOptions
-}
+export default createPlugin({
+  schema: {
+    types: [
+      {
+        title: "Visual Options",
+        type: "string",
+        name: "visualOptions",
+        components: {
+          input: VisualOptions,
+        },
+      },
+    ],
+  },
+})
